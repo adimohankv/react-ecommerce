ae7ffea9ef856deb71e32c7016f9ba8cab996e3f
Adityamohan K V <adityam994@gmail.com>
2020-01-07 16:28:30 +0530

    Added backed for stripe payments

--- client/src/components/stripe-button/stripe-button.js
+++ client/src/components/stripe-button/stripe-button.js
@@ -1,13 +1,26 @@
 import React from 'react';
 import StripeCheckout from 'react-stripe-checkout';
+import axios from 'axios';
 
 const StripeCheckoutButton = ({price}) => {
     const priceInCents = price * 100;
     const publishKey = 'pk_test_GYdAeJiwLVEIlUmphGV9mi9T00zCAWSqUU';
 
     const onToken = function(token) {
-        alert('Payment sucessful!!');
-        console.log(token);
+        axios({
+            url: "payment",
+            method: "POST",
+            data: {
+                amount: price,
+                token: token
+            }
+        }).then(response => {
+            console.log(response);
+            alert('succesful payment');
+        }).catch(error => {
+            console.log('Payment Error: ', error);
+            alert('There was an issue with your payment! Please make sure you use the provided credit card.');
+        });
     };
 
     return (
